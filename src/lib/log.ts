bd65297558a08273c9ac9105d9489e7926de9bea
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-04-26 19:03:33 +0200

    feat: add logLevelHandlerWrapper

---
+++ src/lib/log.ts
@@ -0,0 +1,59 @@
+import { LogLevel as ApifyLogLevel } from 'apify';
+import type { CrawlingContext } from 'crawlee';
+
+import type { ArrVal } from '../utils/types';
+import type { RouteHandlerWrapper } from './router';
+
+export const LOG_LEVEL = ['debug', 'info', 'warn', 'error', 'off'] as const; // prettier-ignore
+export type LogLevel = ArrVal<typeof LOG_LEVEL>;
+
+/** Map log levels of `apify-actor-utils` to log levels of `apify` */
+export const logLevelToApify: Record<LogLevel, ApifyLogLevel> = {
+  off: ApifyLogLevel.OFF,
+  debug: ApifyLogLevel.DEBUG,
+  info: ApifyLogLevel.INFO,
+  warn: ApifyLogLevel.WARNING,
+  error: ApifyLogLevel.ERROR,
+};
+
+/**
+ * Wrapper for Apify route handler that configures log level.
+ *
+ *
+ * Usage with Apify's `RouterHandler.addDefaultHandler`
+ * ```ts
+ * const wrappedHandler = logLevelHandlerWrapper('debug')(handler)
+ * await router.addDefaultHandler<Ctx>(wrappedHandler);
+ * ```
+ *
+ * Usage with Apify's `RouterHandler.addHandler`
+ * ```ts
+ * const wrappedHandler = logLevelHandlerWrapper('error')(handler)
+ * await router.addHandler<Ctx>(wrappedHandler);
+ * ```
+ *
+ * Usage with `createApifyActor`
+ * ```ts
+ * const actor = await createApifyActor<CheerioCrawlingContext>({
+ *   validateInput,
+ *   router: createCheerioRouter(),
+ *   routes,
+ *   routeHandlers: ({ input }) => createHandlers(input!),
+ *   handlerWrappers: ({ input }) => [
+ *     logLevelHandlerWrapper<CheerioCrawlingContext<any, any>>(input?.logLevel ?? 'info'),
+ *   ],
+ *   createCrawler: ({ router, input }) => createCrawler({ router, input, crawlerConfig }),
+ * });
+ * ```
+ */
+export const logLevelHandlerWrapper = <T extends CrawlingContext>(
+  logLevel: LogLevel
+): RouteHandlerWrapper<T> => {
+  return (handler) => {
+    return (ctx, ...args) => {
+      ctx.log.info(`Setting log level to ${logLevel}`);
+      ctx.log.setLevel(logLevelToApify[logLevel]);
+      return handler(ctx, ...args);
+    };
+  };
+};
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,4 @@
 export type MaybePromise<T> = T | Promise<T>;
-
 export type MaybeArray<T> = T | T[];
+
+export type ArrVal<T extends any[] | readonly any[]> = T[number];
