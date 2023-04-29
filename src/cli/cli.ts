9ce9cf33a38b50df737664d218f99130cd0d8524
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-04-29 20:24:43 +0200

    refactor: change migration version from pos to option arg

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -14,7 +14,7 @@ program //
 program
   .command('migrate')
   .description('Run a migration script specified by the version number')
-  .argument('<version>', 'migration version to execute, eg "v1"')
+  .requiredOption('-t --target <target-version>', 'migration version to execute, eg "v1"')
   .requiredOption('-d --dir <path>', 'path to the migrations directory')
   .addHelpText(
     'after',
@@ -23,16 +23,16 @@ program
 Example call:
   $ apify-actor-utils migrate v1`
   )
-  .action(async (version, options) => {
+  .action(async (options) => {
     const migrationsDir = path.resolve(process.cwd(), options.dir);
     const { migrate } = createLocalMigrator({ migrationsDir });
-    await migrate(version);
+    await migrate(options.target);
   });
 
 program
   .command('unmigrate')
   .description('Run an un-migration script specified by the version number')
-  .argument('<version>', 'migration version to execute, eg "v1"')
+  .requiredOption('-t --target <target-version>', 'migration version to execute, eg "v1"')
   .requiredOption('-d --dir <path>', 'path to the migrations directory')
   .addHelpText(
     'after',
@@ -41,10 +41,10 @@ program
 Example call:
   $ apify-actor-utils unmigrate v1`
   )
-  .action(async (version, options) => {
+  .action(async (options) => {
     const migrationsDir = path.resolve(process.cwd(), options.dir);
     const { unmigrate } = createLocalMigrator({ migrationsDir });
-    await unmigrate(version);
+    await unmigrate(options.target);
   });
 
 export const cli = () => {
