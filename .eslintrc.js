352cef3d770c947290ecd547291353d37e0ba9f6
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-05-02 20:40:13 +0200

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/ban-types': 'off',
   },
 };
