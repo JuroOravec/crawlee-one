a07490a6a0115331486d188bd084111143bed5b6
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-05-09 10:16:39 +0200

    refactor: use sentry's "enabled" option

--- src/lib/error/sentry.ts
+++ src/lib/error/sentry.ts
@@ -6,13 +6,15 @@ import * as Sentry from '@sentry/node';
  * By default, sentry is enabled only on Apify server
  * where `process.env.APIFY_IS_AT_HOME` is true
  */
-export const setupSentry = (options: { enabled?: boolean; sentryOptions?: Sentry.NodeOptions }) => {
+export const setupSentry = (sentryOptions?: Sentry.NodeOptions) => {
   // As default, enable sentry only on Apify server
-  const enabled = options.enabled != null ? options.enabled : !!process.env.APIFY_IS_AT_HOME;
+  const enabled =
+    sentryOptions?.enabled != null ? sentryOptions.enabled : !!process.env.APIFY_IS_AT_HOME;
+
   if (!enabled) return;
 
   // We use this field for identification in UI, so it's required.
-  if (!options.sentryOptions?.serverName) throw Error('Sentry setup is missing "serverName" property.'); // prettier-ignore
+  if (!sentryOptions?.serverName) throw Error('Sentry setup is missing "serverName" property.'); // prettier-ignore
 
   Sentry.init({
     dsn: 'https://5b2e0562b4ec4ef6805a3fbbf4ff8acd@o470159.ingest.sentry.io/4505019830370304',
@@ -22,6 +24,6 @@ export const setupSentry = (options: { enabled?: boolean; sentryOptions?: Sentry
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
 
-    ...options.sentryOptions,
+    ...sentryOptions,
   });
 };
